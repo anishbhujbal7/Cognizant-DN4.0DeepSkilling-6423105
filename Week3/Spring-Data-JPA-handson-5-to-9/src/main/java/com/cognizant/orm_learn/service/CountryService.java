5cda77bffcabf000a69118721e0df20c126bd136
Anish Bhujbal <anishbhujbal77@gmail.com>
2025-07-04 17:55:41 +0530

    spring JPA complete

---
+++ Week3/Spring-Data-JPA-handson-5-to-9/src/main/java/com/cognizant/orm_learn/service/CountryService.java
@@ -0,0 +1,34 @@
+package com.cognizant.orm_learn.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import jakarta.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.repository.CountryRepository;
+import com.cognizant.orm_learn.service.exception.CountryNotFoundException;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+
+    @Transactional
+    public Country findCountryByCode(String countryCode) throws CountryNotFoundException {
+        Optional<Country> result = countryRepository.findById(countryCode);
+        if (result.isEmpty()) {
+            throw new CountryNotFoundException("Country with code " + countryCode + " not found");
+        }
+        return result.get();
+    }
+}
